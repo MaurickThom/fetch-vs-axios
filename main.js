3ac2352706dbe3a9893770ce1f9988d4f821f79a
Hume <horacioriverods@gmail.com>
2019-02-21 22:54:17 -0300

    changes

--- main.js
+++ main.js
@@ -1,42 +1,8 @@
 const makeUrl = (url, resource) => url && `${url}/${resource}`
 
-const log = (id) => () => console.log(id)
-
 const whitAxios = (url) => () => axios(url)
 
 const whitFetch = (url) => () => fetch(url)
-
-const getPromedy = (values) => Math.round((values.reduce((a, b) => a + b) / values.length))
-
-const getValuesDifPorcent = (a, b) => {
-    const max = Math.max(a, b)
-    const min = Math.min(a, b)
-    const dif = max - min
-    return Math.round((dif / min) * 100)
-}
-
-const makeBencher = async (log, fn, times) => {
-    const perform = []
-    let startTime = null
-
-    while (times--) {
-        startTime = performance.now()
-        await fn()
-        perform.push(performance.now() - startTime)
-        log()
-    }
-
-    return perform
-}
-
-const repeat = async (fn, times) => {
-    const values = []
-    while (times--) {
-        values.push(await fn())
-    }
-
-    return values
-}
 
 const run = async () => {
     const url = 'http://localhost:3000'
