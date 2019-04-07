ab21331b78e4acdb49aaa41cf880495fa53eea04
Hume <horacioriverods@gmail.com>
2019-04-07 11:41:11 -0300

    fixes

--- bench.js
+++ bench.js
@@ -1,22 +1,36 @@
 const server = require('./server')
 const puppeteer = require('puppeteer')
+const pti = require('puppeteer-to-istanbul')
+const arg = require('arg')
 
-const main = async () => {
+const args = arg({
+    '--req': Number
+    , '--rep': Number
+})
+
+const resources = {
+    foo: 'foo'
+    , image: 'files/image.jpg'
+    , fooJson: 'files/test.json'
+    , imageLarge: 'files/image_large.jpg'
+}
+
+const main = async (resource, requests, repetitions) => {
     const port = 3000
-    const pti = require('puppeteer-to-istanbul')
     await server().listen(port)
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+
     page.on('console', msg => console.log(msg.text()))
+
     await page.coverage.startJSCoverage()
     await page.goto(`http://localhost:${port}/index.html`)
-
-    await page.evaluate(async () => {
-        await main(url(resources.image), 1000, 5)
-    })
+    await page.evaluate((res, reqs, repts) => main(url(res), reqs, repts), resource, requests, repetitions)
 
     const jsCoverage = await page.coverage.stopJSCoverage()
     pti.write(jsCoverage)
     await browser.close()
     process.exit()
 }
+
+main(resources.image, args['--req'], args['--rep'])
